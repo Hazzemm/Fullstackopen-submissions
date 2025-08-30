bd8ec829c0748a2f90ec5301a09cee3700509df9
Hazzemm <hazzemmuhammed@gmail.com>
2025-08-30 18:43:19 +0300

    3.3

--- part3/phonebook/app.js
+++ part3/phonebook/app.js
@@ -20,6 +20,17 @@ app.get('/info',(req,res)=>{
     res.send(`Phonebook has info for ${persons.length} people <br> ${new Date()}`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const personId = +req.params.id
+    const person = persons.find(p=>p.id === personId)
+    if(person){
+        res.send(person)
+    } else {
+        res
+            .send(`<h1>404</h1> <p>person with id: ${personId} is not found</p>`).status(404).end()
+    }
+});
+
 app.listen(3001, () => {
     console.log('Server is running on http://localhost:3000');
 });
